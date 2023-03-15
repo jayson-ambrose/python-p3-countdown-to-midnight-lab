6af4985e2ed6a7102be2baf0fc3b5e51e4beb3ec
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-03-14 23:27:10 -0600

    happy new year

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,24 @@
-# your code goes here!
+import time
+
+def countdown(arg):
+
+    while arg > 0:
+
+        print(f'{arg} SECOND(S)!')
+        arg = arg - 1
+    
+    print("HAPPY NEW YEAR!")
+
+countdown(10)
+
+def countdown_with_sleep(arg):
+
+    while arg > 0:
+
+        print(f'{arg} SECOND(S)!')
+        arg = arg - 1
+        time.sleep(1)
+    
+    print("HAPPY NEW YEAR!")
+
+countdown_with_sleep(5)
